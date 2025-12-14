ff83a820e92eba1f66505e0900cc349de32221b0
AliShSalih9 <alissalih66@gmail.com>
2025-12-14 20:45:34 +0300

    add vercel json file v9

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-base: "Ali-Portofolio",
-  plugins: [
+  base: '/', // Should be '/' for Vercel
+ plugins: [
     
     react({
       
