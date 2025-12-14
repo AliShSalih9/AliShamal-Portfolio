3b681af1b663fc4055e6689fcc9437dfa19a96f1
AliShSalih9 <alissalih66@gmail.com>
2025-12-14 16:08:52 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:process.env.VITE_BASE_PATH|| "/react-vite-deploy",
   plugins: [
+    
     react({
+      
       babel: {
+        
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
