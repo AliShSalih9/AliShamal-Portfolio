f65d9e0d967898ab6ebf245f1390e1334029c04c
AliShSalih9 <alissalih66@gmail.com>
2026-09-11 13:26:41 +0300

    Update portfolio to version 2.3

--- src/contact.tsx
+++ src/contact.tsx
@@ -142,7 +142,7 @@ function contact() {
                 <div className="icon-contact">📧</div>
                 <div className="content-contact">
                   <h5>Email</h5>
-                  <a href="mailto:ali.shamal.code@gmail.com?subject=Contact&body=Hello%20Ali%2C%20I%20want%20to%20message%20you">
+                  <a href="mailto:ali.shamal.dev@gmail.com?subject=Contact&body=Hello%20Ali%2C%20I%20want%20to%20message%20you">
                     ali.shamal.code@gmail.com
                   </a>
                 </div>
